59ce628cfb7c04ec816538d8dd3611b2d56cea30
Brandon Yates <BTYates@student.fullsail.edu>
2023-11-15 16:00:06 -0500

    Got Add button into the UI

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import '../constants/colors.dart';
 import '../widgets/todo_items.dart';
-import 'package:tododemo/model/todo.dart';
+import '../model/todo.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final todoList = ToDo.todoList();
 
   @override
@@ -13,34 +18,90 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: appBackBlue,
       appBar: _buldAppBar(),
-      body: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20, 
-          vertical: 15),
-        child: Column(
-          children: [
-            searcBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 50, bottom: 20),
-                    child: const Text(
-                      'ToDos',
-                      style: TextStyle(
-                        fontSize: 30, fontWeight: FontWeight.w500,
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20, 
+              vertical: 15),
+            child: Column(
+              children: [
+                searcBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 20),
+                        child: const Text(
+                          'ToDos',
+                          style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.w500,
+                          ),
+                        ),
                       ),
-                    ),
+                      for ( ToDo toDoo in todoList)
+                        ToDoItem(todo: toDoo,),
+                    ],
+                  )
+                )
+              ]
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(
+                  bottom: 20,
+                  right: 20,
+                  left: 20,
                   ),
-                  for ( ToDo toDoo in todoList)
-                    ToDoItem(todo: toDoo,),
-                ],
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [BoxShadow(
+                      color: Colors.grey, 
+                      offset: Offset(0.0,0.0),
+                      blurRadius: 10.0,
+                      spreadRadius: 0,
+                      ),],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Add new todo here',
+                      border: InputBorder.none
+                    ),)
+                )
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  bottom: 20,
+                  right: 20,
+                ),
+                child: ElevatedButton(
+                  child: Text('+',
+                  style: TextStyle(fontSize: 40, ),),
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: styleGreen,
+                    minimumSize: Size(60, 60),
+                    elevation: 10,
+                  )
+                ),
               )
-            )
-          ]
-        ),
+            ]),
+          )
+        ],
       ),
     );
+  }
+
+  void _handleToDoChange(){
+    
   }
 
   Widget searcBox() {
